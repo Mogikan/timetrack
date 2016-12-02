b3fe15029d70a829adcdff69c50f1632c59b4483
Vitaliy Korney <Vitaliy.Korney@enterra-inc.com>
2016-12-02 19:18:18 +0700

    servicehost + uwp client

---
+++ TimeTrack.Client.UWP/EmployeesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ASTU.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTrack.Client.UWP.TimeTrackService;
+
+namespace TimeTrack.Client.UWP
+{
+    public class EmployeesViewModel : Observable
+    {
+        public EmployeesViewModel()
+        {
+            var binding = new BasicHttpBinding();
+            _serviceClient = new TimeTrackService.TimeTrackServiceClient(binding, new EndpointAddress(new Uri("http://localhost:9999/TimetrackService")));
+            LoadData();
+        }
+        private TimeTrackService.TimeTrackServiceClient _serviceClient;
+        public async void LoadData()
+        {
+            Employees = await _serviceClient.GetEmployeesAsync();
+        }
+        ObservableCollection<Employee> _employees;
+        public ObservableCollection<Employee> Employees
+        {
+            get
+            {
+                return _employees;
+            }
+            set
+            {
+                _employees = value;
+                NotifyPropertyChanged(() => Employees);
+            }
+        }
+    }
+}
---
+++ TimeTrack.Service.ConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTrack.BusinessLogic;
+
+namespace TimeTrack.Service.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeTrackHost = new ServiceHost(typeof(TimeTrackService));
+            timeTrackHost.Open();
+            Console.WriteLine("serviceStarted");
+            Console.ReadKey();
+            timeTrackHost.Close();
+        }
+    }
+}
